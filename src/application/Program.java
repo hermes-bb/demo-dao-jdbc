eee97326ea606fb4e96b6b333ebb57a5f402d757
Hermes Belarmino Barbosa <hermes-bb@hotmail.com>
2024-03-20 09:55:06 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,10 +9,12 @@ public class Program {
 	public static void main(String[] args) {
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-
+		
+		System.out.println("=== Begin TEST 1: seller findById ===");
 		Seller seller = sellerDao.findById(3);
 
 		System.out.println(seller);
+		System.out.println("=== End   TEST 1: seller findById ===");
 
 	}
 
